5e821f5fe382c9d9c1d022925b1ce985fd7f17f3
Lucid <bluecho97@gmail.com>
2025-10-10 03:28:55 +0900

    Initial Commit

---
+++ SwiftUIDocument/SwiftUIDocumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDocumentApp.swift
+//  SwiftUIDocument
+//
+//  Created by Lucid on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDocumentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIDocumentTests/SwiftUIDocumentTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDocumentTests.swift
+//  SwiftUIDocumentTests
+//
+//  Created by Lucid on 10/10/25.
+//
+
+import Testing
+@testable import SwiftUIDocument
+
+struct SwiftUIDocumentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
